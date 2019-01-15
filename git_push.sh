34e20bdc69730f72ce860afe6a55c4d9d3e86e34
kiran20v <kiran20v@gmail.com>
2019-01-15 19:54:01 +0530

    create cust with hib, ang with view

---
+++ git_push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#git config --global user.name kiran20v
+#git config --global user.email kiran20v@gmail.com
+#git init python_projects
+git add *
+echo "Commit message:"
+read commit_msg
+git commit -m "$commit_msg"
+git remote add python_projects https://github.com/kiran20v/Grails3_Ang6_QuartzUI
+git push -u Grails3_Ang6_QuartzUI --all
+#git show
+echo "Pushed changes to Github"
